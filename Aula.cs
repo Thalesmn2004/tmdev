9d5957b6dbb79ae19bb0339049aaf968b64f2b19
Thalesmn2004 <thalesmn2017@gmail.com>
2023-03-03 15:08:52 -0300

    ok

---
+++ Aula.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PrimeiroProjeto
+{
+    internal class progam
+    {
+        static void Main(string[] args)
+    {
+        Console.WhriteLine("Eai fih");    
+    }
+
+    }
+
+
+}
